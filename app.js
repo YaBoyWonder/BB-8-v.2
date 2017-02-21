2f477c6b2576fef2030131d2665613db46588b61
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-02-21 20:29:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -43,28 +43,8 @@ client.reload = command => {
 };
 
 client.elevation = message => {
-  
-     
-  let permlvl = 0;
-  let mod_role = message.guild.roles.find('name', settings.modrolename);
-  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
-  let admin_role = message.guild.roles.find('name', settings.adminrolename);
-  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
-  if (message.author.id === settings.ownerid) permlvl = 4;
-  return permlvl;
+
 };
 
-
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// REMINDER, WONDER REGEX THE regToken!
-
-
-client.on('warn', e => {
-  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
-});
-
-client.on('error', e => {
-  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
-});
 
 client.login(settings.token);
