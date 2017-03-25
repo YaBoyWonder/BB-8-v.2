b80a11d6b001fdfd150ca8f2e5319e6dfe43d34d
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-24 22:50:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ client.reload = command => {
 
 client.util = {
   embed: require('../util/embed'),
-  fetchStats: require('../util/fetchStats')(client),
+   fetchStats: require('../util/fetchStats')(client),
     isStaff: require('../util/isStaff')
 }
 
