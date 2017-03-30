eb16a57afa769c1eb2bc8e03e5dcbaed88f3a64a
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-30 00:34:18 -0400

    Delete site.pp

--- site.pp
+++
@@ -1,7 +0,0 @@
-$basedir = "/tmp/hadoop-example"
-class { "hadoop::basedir": path => $basedir }
-
-hadoop::disk {
-  [ "/dev/sdb1", "/dev/sdc1", "/dev/sdd1", "/dev/sde1" ]: 
-    basedir => $basedir;
-}
