de20b51a461d1a561fd59a904d25aa71bd77022f
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-31 06:18:47 -0400

    Add files via upload

---
+++ commands/owner.js
@@ -0,0 +1,11 @@
+exports.run = function(client, message, args) {
+  message.channel.sendMessage("", {embed: {
+    color: 3447003,
+    title: message.guild.owner.user.username,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: '©BB-8'
+    }
+  }});
+}
