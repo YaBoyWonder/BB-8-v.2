aca8de46085d0802789a15060f9594571aa14e07
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-17 19:53:53 -0400

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -2,4 +2,4 @@ const reqEvent = (event) => require(`../events/${event}`)
 module.exports = client => { 
 client.on('ready', () => reqEvent('ready')(client));
 client.on('message', reqEvent('message'));
-};
+ };
