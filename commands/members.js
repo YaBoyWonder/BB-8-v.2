554f753d4ef07cf490f874554dd6ec62d6af792b
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-22 08:11:11 -0400

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -2,5 +2,9 @@ exports.run = function(client, message, args) {
   message.channel.sendMessage("", {embed: {
     color: 0x00FF7F,
     title: message.guild.memberCount,
+      footer: {
+      icon_url: client.user.avatarURL,
+      text: 'Wonder ❤'
+    }
   }});
 }
