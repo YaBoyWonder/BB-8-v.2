28bd9e6e5d849a22407932c8c59986d355d24457
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-22 08:10:56 -0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,11 +1,1 @@
-exports.run = function(client, message, args) {
-  message.channel.sendMessage("", {embed: {
-    color: 3447003,
-    title: message.guild.owner.user.username,
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: 'Wonder ❤'
-    }
-  }});
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('9.a=b(1,0,8){0.c.7("",{3:{5:6,4:0.n.d.2.l,m:j i(),e:{f:1.2.g,h:\'k ❤\'}}})}',24,24,'message|client|user|embed|title|color|3447003|sendMessage|args|exports|run|function|channel|owner|footer|icon_url|avatarURL|text|Date|new|Wonder|username|timestamp|guild'.split('|'),0,{}))
