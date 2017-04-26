1cf557e94c7d601a66df1318a6133114be0afc62
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-25 20:57:33 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,6 +20,11 @@ name: "ping",
 value: `returns ping in ms`,
 inline: true
 },
+  {
+name: "coin",
+value: `flips a coin for heads or tails`,
+inline: true
+},
 {
  name: "region",
  value: `shows the region of the guild`,
