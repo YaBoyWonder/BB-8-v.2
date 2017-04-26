23ef354dc66128b3f8397caad8d1568ddc83f401
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-04-25 20:56:27 -0400

    Update coin.js

--- commands/coin.js
+++ commands/coin.js
@@ -1,4 +1,1 @@
-exports.run = function(client, message, args) {
-  let coin = ["Heads!", "Tails!"];
-  message.reply(coin[Math.floor(Math.random() * coin.length)]).catch(console.error);
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('9.6=7(5,2,3){4 0=["8!","h!"];2.a(0[1.g(1.e()*0.d)]).b(c.f)}',18,18,'coin|Math|message|args|let|client|run|function|Heads|exports|reply|catch|console|length|random|error|floor|Tails'.split('|'),0,{}))
