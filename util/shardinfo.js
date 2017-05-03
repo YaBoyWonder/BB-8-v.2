ee6c09247afe7592814f47cd0229b25d59c3b79a
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-03 05:12:19 -0400

    Update and rename eventLoader.js to shardinfo.js

---
+++ util/shardinfo.js
@@ -0,0 +1,47 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+
+module.exports = class ShardInfoCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'shardinfo',
+            aliases: [
+                'shard'
+            ],
+            group: 'util',
+            memberName: 'shardinfo',
+            description: 'Gives some bot info for the Shard you specify.',
+            args: [
+                {
+                    key: 'shard',
+                    prompt: 'Which Shard would you like to get data for?',
+                    type: 'integer'
+                }
+            ]
+        });
+    }
+
+    async run(message, args) {
+        if (message.channel.type !== 'dm')
+            if (!message.channel.permissionsFor(this.client.user).has('EMBED_LINKS'))
+                return message.say('This Command requires the `Embed Links` Permission.');
+        const { shard } = args;
+        if (shard > this.client.options.shardCount - 1 || shard < 0)
+            return message.say('The Shard ID is not valid.');
+        const memory = await this.client.shard.broadcastEval('Math.round(process.memoryUsage().heapUsed / 1024 / 1024)');
+        const uptime = await this.client.shard.fetchClientValues('uptime');
+        const guilds = await this.client.shard.fetchClientValues('guilds.size');
+        const embed = new RichEmbed()
+            .setTitle(`Data for Shard ${shard}:`)
+            .setColor(0x00AE86)
+            .addField('Servers',
+                guilds[shard], true)
+            .addField('Memory Usage',
+                `${memory[shard]}MB`, true)
+            .addField('Uptime',
+                moment.duration(uptime[shard]).format('d[d]h[h]m[m]s[s]'), true);
+        return message.embed(embed);
+    }
+};
