6d9513fd11d5e1de38926c3dde6dca410f3dc167
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-03 05:26:49 -0400

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,3 +1,1 @@
- exports.run = function(client, message, args) {
-		message.reply("Here is my developer's git: https://github.com/YaBoyWonder")
-	}
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5.6=4(3,0,1){0.2("7 8 e f\'d c: 9://a.b/g")}',17,17,'message|args|reply|client|function|exports|run|Here|is|https|github|com|git|s|my|developer|YaBoyWonder'.split('|'),0,{}))
