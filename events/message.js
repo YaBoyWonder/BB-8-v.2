df277873081c0c70d835806805a17205477754a2
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-10 18:58:48 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,7 +5,7 @@ if (message.author.bot) return;
   const client = message.client;
   var prefix = "YOUR_BOT_PREFIX"
    const args = message.content.split(' ');
-  const command = args.shift().slice(settings.prefix.length);
+   const command = args.shift().slice(settings.prefix.length);
   try{
     let cmdFile = require(`./commands/${command}`);
     cmdFile.run(client, message, args);
