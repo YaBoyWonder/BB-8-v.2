cb0e74785a02ca48b997b6e1dd5b39dc22d1af58
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-10 22:11:10 -0400

    Update analysis.ts

--- class/analysis.ts
+++ class/analysis.ts
@@ -3,12 +3,12 @@ const client = new CommandClient({
 unknownCommandResponse: true
 });
 exports.run = function(client, message, args) {
-	message.channel.sendMessage('BB-8 is online and ready to go');	
+  message.channel.sendMessage('BB-8 is online and ready to go');	
 });
 	public hasPermission(message: CommandMessage): boolean {
 			return true;
 	}
-	public async run(message: CommandMessage, args: {} | string | string[]): Promise<Message | Message[]> {
+	    public async run(message: CommandMessage, args: {} | string | string[]): Promise<Message | Message[]> {
 		return message.say('Test Online');
 	}
 }
