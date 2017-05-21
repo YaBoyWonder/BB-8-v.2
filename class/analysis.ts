eb70830b834d14101c731cf328c52d1938de5953
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-21 15:02:06 -0400

    Update analysis.ts

--- class/analysis.ts
+++ class/analysis.ts
@@ -3,7 +3,7 @@ const client = new CommandClient({
 unknownCommandResponse: true
 });
 exports.run = function(client, message, args) {
-  message.channel.sendMessage('BB-8 is online and ready to go');	
+  message.channel.sendMessage('BB-8 is online and ready to function');	
 });
 	public hasPermission(message: CommandMessage): boolean {
 			return true;
