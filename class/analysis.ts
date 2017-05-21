46e11d57cad6d6bd3f32cab3509d82ece7d3fde0
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-21 15:02:18 -0400

    Update analysis.ts

--- class/analysis.ts
+++ class/analysis.ts
@@ -6,7 +6,7 @@ exports.run = function(client, message, args) {
   message.channel.sendMessage('BB-8 is online and ready to function');	
 });
 	public hasPermission(message: CommandMessage): boolean {
-		return true;
+		 return true;
 	}
 	    public async run(message: CommandMessage, args: {} | string | string[]): Promise<Message | Message[]> {
 		return message.say('Test Online');
